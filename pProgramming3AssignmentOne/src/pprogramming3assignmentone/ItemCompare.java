c775e5bbf0aabb409835967d17deddc38b13ca01
rNdm74 <rndm@windowslive.com>
2013-05-23 15:17:32 +1200

    Adding to repo

---
+++ pProgramming3AssignmentOne/src/pprogramming3assignmentone/ItemCompare.java
@@ -0,0 +1,16 @@
+
+package pprogramming3assignmentone;
+
+import java.util.Comparator;
+import javax.swing.JFormattedTextField;
+
+/**
+ *
+ * @author rndm
+ */
+public class ItemCompare implements Comparator<JFormattedTextField> {
+    @Override
+    public int compare(JFormattedTextField o1, JFormattedTextField o2) {
+        return 0;        
+    }    
+}
