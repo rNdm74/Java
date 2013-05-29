ca9bc8b9f218663fee2b8688b4e2757487127f93
rNdm74 <rndm@windowslive.com>
2013-05-29 19:37:10 +1200

    Update GUI

---
+++ pProgramming3AssignmentOne/src/pprogramming3assignmentone/Sorting.java
@@ -0,0 +1,226 @@
+
+package pprogramming3assignmentone;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
+
+/**
+ *
+ * @author rndm
+ */
+public class Sorting extends javax.swing.JPanel implements Comparable<Object>{
+
+    public Sorting(Home home) {
+        this.home = home;
+        
+        initComponents();
+        
+        list.removeAllItems();
+        
+        for (Object s: home.csvData.getData().get(0)) {
+            list.addItem(((String)s).toUpperCase());
+        }
+               
+        data = new ArrayList<>();
+        
+        int length = home.csvData.getData().get(0).length;
+
+        for (int column = 0; column < length; column++) {
+            String[] objects = new String[home.csvData.getData().size()];
+
+            for (int row = 0; row < home.csvData.getData().size(); row++) {
+                objects[row] = (String) home.csvData.getData().get(row)[column];
+            }
+
+            data.add(objects);
+        }
+        
+        Arrays.sort(data.get(0));
+        
+        populateTable();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        list = new javax.swing.JComboBox();
+        tableScrollPane = new javax.swing.JScrollPane();
+        table = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
+        jRadioButton1 = new javax.swing.JRadioButton();
+        jRadioButton2 = new javax.swing.JRadioButton();
+
+        setBackground(new java.awt.Color(254, 254, 254));
+
+        list.setMaximumRowCount(100);
+        list.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item", " " }));
+        list.setEnabled(false);
+        list.setName("list"); // NOI18N
+        list.setPreferredSize(new java.awt.Dimension(47, 25));
+        list.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                listItemStateChanged(evt);
+            }
+        });
+        list.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                listActionPerformed(evt);
+            }
+        });
+
+        tableScrollPane.setBorder(null);
+
+        table.setAutoCreateRowSorter(true);
+        table.setBorder(null);
+        table.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null}
+            },
+            new String [] {
+                "Title 1"
+            }
+        ));
+        table.setGridColor(java.awt.Color.white);
+        table.setIntercellSpacing(new java.awt.Dimension(0, 0));
+        tableScrollPane.setViewportView(table);
+
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pprogramming3assignmentone/16x16/filter.png"))); // NOI18N
+        jButton1.setEnabled(false);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        buttonGroup1.add(jRadioButton1);
+        jRadioButton1.setSelected(true);
+        jRadioButton1.setText("Overview");
+
+        buttonGroup1.add(jRadioButton2);
+        jRadioButton2.setText("Single Item");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(tableScrollPane, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jRadioButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jRadioButton2)
+                        .addGap(12, 12, 12)
+                        .addComponent(list, 0, 179, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton1)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(list, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jRadioButton2)
+                        .addComponent(jRadioButton1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(tableScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 203, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton1)))
+                .addContainerGap())
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void listItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_listItemStateChanged
+
+    }//GEN-LAST:event_listItemStateChanged
+
+    private void listActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_listActionPerformed
+
+        }
+
+        private void populateTable() {
+            DefaultTableModel model = (DefaultTableModel)table.getModel();
+            
+            model.setColumnCount(0);
+            model.setRowCount(0);
+
+            TableRowSorter sorter = new TableRowSorter();
+            table.setRowSorter(sorter);
+
+            sorter.setModel(table.getModel());
+                
+            for (int column = 0; column < list.getItemCount(); column++) {
+                                
+                int pos = list.getSelectedIndex();
+                
+                ArrayList<Object> columnData = new ArrayList<>();
+                                
+                for (int row = 1; row < data.get(pos).length; row++) {
+                    
+                    Object item = data.get(column)[row];
+                    
+                    if (isValid(item)) {
+                        columnData.add(Double.parseDouble((String)item));
+                    }
+                    else{
+                        columnData.add((String)item);
+                    }
+                }
+                
+                model.addColumn(list.getItemAt(column),columnData.toArray());
+            }   
+            
+            for (int column = 0; column < list.getItemCount(); column++) {
+                if (isValid(data.get(column)[1])) {
+                    sorter.setComparator(column, new CompareDouble());
+                }
+                else{
+                    sorter.setComparator(column, new CompareString());
+                }
+            }
+    }//GEN-LAST:event_listActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        populateTable();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    public boolean isValid(Object input){ 
+       try  
+       {  
+          Double.parseDouble((String) input);  
+          return true;  
+       }  
+       catch( Exception e)  
+       {  
+          return false;  
+       }  
+    }
+        
+    private ArrayList<String[]> data;   
+    private Home home;
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JRadioButton jRadioButton1;
+    private javax.swing.JRadioButton jRadioButton2;
+    private javax.swing.JComboBox list;
+    private javax.swing.JTable table;
+    private javax.swing.JScrollPane tableScrollPane;
+    // End of variables declaration//GEN-END:variables
+
+    @Override
+    public int compareTo(Object o) {
+        return Integer.parseInt(data.get(0)[0]);
+    }
+}
