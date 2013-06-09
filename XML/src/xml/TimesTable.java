dfa454157529712fdf0fa9d3a004813c694a3e0c
rNdm <rndm@windowslive.com>
2013-06-09 22:52:34 +1200

    Adding to repo

---
+++ XML/src/xml/TimesTable.java
@@ -0,0 +1,29 @@
+
+package xml;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rNdm
+ */
+public class TimesTable {
+
+    private String name;
+    
+    private ArrayList<Equation> timesTable;
+    
+    public TimesTable(String name){
+        this.name = name;
+        timesTable = new ReadXML("tables.xml").getTableData(name);   
+    }    
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Equation> getTimesTable() {
+        return timesTable;
+    }
+
+}
